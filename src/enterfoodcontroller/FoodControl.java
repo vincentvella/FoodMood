9399589e1b4b184a4bcb8f2817005f9502d531f6
Vincent Vella <vincevella0@gmail.com>
2017-09-17 23:29:43 -0400

    Quick update for you guys

---
+++ src/enterfoodcontroller/FoodControl.java
@@ -0,0 +1,25 @@
+
+package enterfoodcontroller;
+
+import enterfoodmoodmodel.Food;
+import enterfoodmoodmodel.Mood;
+
+public class FoodControl {
+    
+    public void updateFoodName (String foodName) {
+
+    }
+    
+    public void updateTimeEntered (String timeEntered) {
+
+    }
+    
+    public void updateFoodQuantity (String foodQuantity) {
+
+    }
+    
+    public void updateFoodMood (Mood mood) {
+        
+    }
+    
+}
