3c4f85abd323b1b3ffa3a569f4e87c015de7cd95
Joe <Joe@DESKTOP-7MVAE7V>
2017-09-17 22:10:31 -0400

    Rename

---
+++ src/userprofilemodel/Profile.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userprofilemodel;
+
+/**
+ * This class holds all the relevant data someone will have on their profile
+ * @author Joe
+ */
+public class Profile {
+    private User user;
+    
+    /**
+     * This is the default constructor for a profile
+     */
+    public Profile(){
+        
+    }
+    
+    private void setUser(User username){
+        
+    }
+    
+}
