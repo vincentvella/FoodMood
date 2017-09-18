3b4cd8afa8b7b1e2252ae2fd28877a9fe1632bba
Vincent Vella <vincevella0@gmail.com>
2017-09-17 23:34:37 -0400

    Finished Food Control class

--- src/enterfoodcontroller/FoodControl.java
+++ src/enterfoodcontroller/FoodControl.java
@@ -5,19 +5,31 @@ import enterfoodmoodmodel.Food;
 import enterfoodmoodmodel.Mood;
 
 public class FoodControl {
-    
+    /**
+     * This method will update the food name in the model
+     * @param foodName 
+     */
     public void updateFoodName (String foodName) {
 
     }
-    
+    /**
+     * This method will update the time entered in the model
+     * @param timeEntered 
+     */
     public void updateTimeEntered (String timeEntered) {
 
     }
-    
+    /**
+     * This method will update the time entered in the model
+     * @param foodQuantity 
+     */
     public void updateFoodQuantity (String foodQuantity) {
 
     }
-    
+    /**
+     * This method will update the mood associated with a food
+     * @param mood 
+     */
     public void updateFoodMood (Mood mood) {
         
     }
