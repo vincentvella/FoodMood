c3b49f14dd6205d4e44eb36112d4746f125f5cca
Zach <zzt5046@psu.edu>
2017-09-17 23:00:59 -0400

    added userprofilecontroller
    setUserInfo, setUserSettings

---
+++ src/userprofilecontroller/setUserInfo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userprofilecontroller;
+
+import userprofilemodel.Profile;
+/**
+ *
+ * @author Zach
+ */
+public class setUserInfo {
+    
+    /**
+     * This is the default constructor for the setUserInfo class
+     */
+    public setUserInfo(){
+        
+    }
+    
+    /**
+     * This sets a user's email address to a new value if authenticated
+     * @param username The user who's setting are affected
+     * @param email Email value to replace with
+     */
+    public void setEmail(Profile username, String email){
+        
+    }
+}
---
+++ src/userprofilecontroller/setUserSettings.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userprofilecontroller;
+
+import userprofilemodel.Profile;
+/**
+ *
+ * @author Zach
+ */
+public class setUserSettings {
+    
+    /**
+     * This is the default constructor for setUserSettings
+     */
+    public setUserSettings(){
+        
+    }
+    
+    /**
+     * This sets a user's email address to a new value if authenticated
+     * @param username The user who's setting are affected
+     * @param x Integer representing yes(1) or no(0)
+     */
+    public void setReceiveNotifications(Profile username, int x){
+        
+    }
+}
