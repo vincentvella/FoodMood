c6fc3f9e2cb6eaba8fa60e0941155c4a3f2c2b56
Joe <Joe@DESKTOP-7MVAE7V>
2017-09-17 21:49:24 -0400

    Rename modules to be consistent with MVC example

--- src/enterfoodmoodmodel/Food.java
+++ src/enterfoodmoodmodel/Food.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package enterfoodmood;
+package enterfoodmoodmodel;
+
+import enterfoodmoodmodel.Mood;
 
 /**
  * This contains all the attributes and methods that foods entered into the app 
