f6a050cfead428d22586874e73e80ec4598fa4a7
Vincent Vella <vincevella0@gmail.com>
2017-09-17 23:56:43 -0400

    Last file pushed

---
+++ src/foodmoodcontroller/StatController.java
@@ -0,0 +1,16 @@
+package foodmoodcontroller;
+
+import foodmoodmodel.*;
+
+public class StatController {
+    private FoodStat foodStat;
+    private MoodStat moodStat;
+    
+    public FoodStat updateFoodStats(FoodStat foodstat) {
+        return foodStat;
+    }
+    
+    public MoodStat updateMoodStats(MoodStat moodstat) {
+        return moodStat;
+    }
+}
