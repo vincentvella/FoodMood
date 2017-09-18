658bffda212382389c99042cf0096d9d6a7eeeb9
Joe <Joe@DESKTOP-7MVAE7V>
2017-09-17 21:41:41 -0400

    Add basic Food and Mood classes

---
+++ src/enterfoodmood/Food.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enterfoodmood;
+
+/**
+ * This contains all the attributes and methods that foods entered into the app 
+ * will have associated with them.
+ * @author Joe
+ */
+public class Food {
+    private String foodName;
+    private String timeEntered;
+    private double foodQuantity;
+    private Mood foodMood;
+    /**
+     * This is the default constructor for the Food class
+     */
+    public Food(){
+        
+    }
+    
+    /**
+     * Method to get the name of the food object as a string
+     * @return Name of the type of food
+     */
+    public String getFoodName(){
+        return foodName;
+    }
+    
+    /**
+     * Method to get the time the food object was entered
+     * @return The time the food object was entered as a string value
+     */
+    public String getTimeEntered(){
+        return timeEntered;
+    }
+    
+    /**
+     * Method to get the name of the food object as a string
+     * @return Name of the type of food
+     */
+    public double getFoodQuantity(){
+        return foodQuantity;
+    }
+    
+    /**
+     * Gets the mood the user entered associated with this food object
+     * @return The mood that was entered in association with this food object
+     */
+    public Mood getFoodMood(){
+        return foodMood;
+    }
+}
