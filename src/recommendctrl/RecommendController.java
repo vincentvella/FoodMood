a716c4ee6505d00608e4a504bfed0aa4c14801fa
Vincent Vella <vincevella0@gmail.com>
2017-10-01 23:20:18 -0400

    Recommendation Output Added

--- src/recommendctrl/RecommendController.java
+++ src/recommendctrl/RecommendController.java
@@ -17,10 +17,11 @@ public class RecommendController {
     public RecommendController(){
         RecommendView recommendView = new RecommendView();
         RecommendModel recommendModel = new RecommendModel();
-        System.out.println("New RecommendController instantiated.");
+        System.out.println("`-Recommend Controller instantiated.");
+        calculateRecommend(null, null);
     }
     
     public void calculateRecommend(Food f, Mood m){
-        
+        System.out.println("  `--Recommendations Calculated");
     }
 }
