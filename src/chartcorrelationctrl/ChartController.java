a21efef339f6923e4a2f780355ee184bcef1fc8e
Vincent Vella <vincevella0@gmail.com>
2017-10-01 22:46:54 -0400

    Fixes Chart Cntl

--- src/chartcorrelationctrl/ChartController.java
+++ src/chartcorrelationctrl/ChartController.java
@@ -16,16 +16,16 @@ public class ChartController {
      * Constructor for ChartController object
      */
     public ChartController(){
-        
+        displayView();
     }
     
     /**
      * Initializes a ChartView object
      */
     public void displayView () {
+        System.out.println("`--Instantiating Chart");
         Chart chart = new Chart();
+        System.out.printf("`--Chart Instantiated\n`--Building Chart UI\n");
         ChartView chartUI = new ChartView(chart);
     }
-    
-    
 }
