08456e7001f2267ef307472c3311837434856924
Joe <Joe@DESKTOP-7MVAE7V>
2017-10-01 21:55:08 -0400

    Update stats packages slightly (naming + flow of control

--- src/statsctrl/StatController.java
+++ src/statsctrl/StatController.java
@@ -2,16 +2,30 @@ package statsctrl;
 
 import statsmodel.MoodStat;
 import statsmodel.FoodStat;
+import statsview.StatsView;
 
 public class StatController {
     private FoodStat foodStat;
     private MoodStat moodStat;
     
-    public FoodStat updateFoodStats(FoodStat foodstat) {
-        return foodStat;
+    public StatController(FoodStat food, MoodStat mood){
+        updateFoodStats(food);
+        updateMoodStats(mood);
+        StatsView statsView = new StatsView();
     }
     
-    public MoodStat updateMoodStats(MoodStat moodstat) {
-        return moodStat;
+    /**
+     * Updates food stats
+     * @param foodstat The foodstat being added
+     */
+    public void updateFoodStats(FoodStat foodstat) {
+        
+    }
+    /**
+     * Updates mood stats
+     * @param moodstat The moodstat being added
+     */
+    public void updateMoodStats(MoodStat moodstat) {
+        
     }
 }
