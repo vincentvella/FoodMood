a0cf3f042c3fa9d17f53d636f0b091083237df51
Vincent Vella <vincevella0@gmail.com>
2017-10-01 23:23:57 -0400

    Added Relationship Output

--- src/relationsctrl/RelationsController.java
+++ src/relationsctrl/RelationsController.java
@@ -18,10 +18,11 @@ public class RelationsController {
     public RelationsController(){
         RelationsView relationsView = new RelationsView();
         RelationsModel relationsModel = new RelationsModel();
-        System.out.println("New RelationsController instantiated.");
+        System.out.println("`-New RelationsController instantiated.");
+        addRelationsEntry(null,null);
     }
     
     public void addRelationsEntry(Food f, Mood m){
-        
+        System.out.println("  `--Adding a relationship entry");
     }
 }
