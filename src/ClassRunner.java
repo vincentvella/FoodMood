c462fdc6f50ffb9574dc0c20d8488c52013e6452
Joe <Joe@DESKTOP-7MVAE7V>
2017-10-29 21:51:40 -0400

    Merge origin/master
    Conflicts:
    	src/crudfoodview/FoodView.java

---
+++ src/ClassRunner.java
@@ -0,0 +1,13 @@
+import loginview.LoginPanel;
+import userprofilectrl.UserProfileCtrl;
+
+public class ClassRunner {
+            /**
+        * @param args the command line arguments
+        */
+       public static void main(String args[]) {
+           
+           UserProfileCtrl ctrl = new UserProfileCtrl();
+           
+       }
+}
