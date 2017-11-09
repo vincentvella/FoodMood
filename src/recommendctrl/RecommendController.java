8709a3a6a9fc329799779ab2ea8d61993c7d9cd7
Vincent Vella <vincevella0@gmail.com>
2017-11-08 23:27:27 -0500

    Remove unncesssary comments

--- src/recommendctrl/RecommendController.java
+++ src/recommendctrl/RecommendController.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package recommendctrl;
 import recommendview.RecommendView;
 import recommendmodel.RecommendModel;
 import crudfoodmodel.Food;
 import crudmoodmodel.Mood;
-/**
- *
- * @author Joe
- */
+
 public class RecommendController {
     
     public RecommendController(){
--- src/userprofileview/SettingsView.java
+++ src/userprofileview/SettingsView.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package userprofileview;
 import userprofilemodel.*;
 import userprofilectrl.UserProfileCtrl;
-/**
- *
- * @author Zach
- */
+
 public class SettingsView extends javax.swing.JFrame{
     
     private javax.swing.JButton jButton1;
