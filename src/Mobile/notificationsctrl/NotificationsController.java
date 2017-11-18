8c52707081ca9e62266c1bdaa3c5eafcce608e76
benthekatz <benthekatz@gmail.com>
2017-11-17 20:47:53 -0500

    "Timer Started" output

--- src/Mobile/notificationsctrl/NotificationsController.java
+++ src/Mobile/notificationsctrl/NotificationsController.java
@@ -25,7 +25,8 @@ public class NotificationsController {
 
     public void startTimer() {
         //120000 for 2 minutes
-        timer.schedule(task, 120000);
+        System.out.println("`-Timer Started: Reminder in 2 Minutes");
+        timer.schedule(task, 12000);
     }
 //    public void newNotification(int type, String message){
 //        //types
